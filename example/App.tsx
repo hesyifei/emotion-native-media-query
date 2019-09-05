e25a0a2d72093515f9c6ff3f32bbf5bcb61ce511
Yifei He <yifei@hesyifei.com>
2019-09-04 18:46:29 -0700

    Add basic Expo example

---
+++ example/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View } from 'react-native';
+import styled from "@emotion/native";
+
+const Title = styled.Text({
+  backgroundColor: "#FF0018",
+  fontSize: 100
+});
+
+export default function App() {
+  return (
+    <View>
+      <Title>Open up App.tsx to start working on your app!</Title>
+    </View>
+  );
+}
