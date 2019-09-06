7b9c971329a2aa1b9bbaa87047a486cf452a69be
Yifei He <yifei@hesyifei.com>
2019-09-06 12:59:35 -0700

    Add test for MinWidth and MaxWidth

--- __tests__/RView-test.tsx
+++ __tests__/RView-test.tsx
@@ -3,6 +3,24 @@ import { Text } from "react-native";
 import renderer from "react-test-renderer";
 import RView, { MediaRule } from "../src";
 
+const widthList: number[] = [
+  1,
+  100,
+  299,
+  300,
+  301,
+  450,
+  599,
+  600,
+  601,
+  750,
+  899,
+  900,
+  901,
+  1000,
+  10000
+];
+
 jest.mock("Dimensions");
 
 describe("RView", () => {
@@ -16,23 +34,7 @@ describe("RView", () => {
     expect(tree).toMatchSnapshot();
   });
 
-  test.each([
-    1,
-    100,
-    299,
-    300,
-    301,
-    450,
-    599,
-    600,
-    601,
-    750,
-    899,
-    900,
-    901,
-    1000,
-    10000
-  ])("simple responsive with width %ipx", width => {
+  test.each(widthList)("simple MaxWidth with width %ipx", width => {
     require("Dimensions").__setDimensions(width, 736);
     const component = renderer.create(
       <RView
@@ -49,6 +51,43 @@ describe("RView", () => {
             },
             900: {
               backgroundColor: "blue"
+            }
+          }
+        }}
+      />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test.each(widthList)("simple MinWidth and MaxWidth with width %ipx", width => {
+    require("Dimensions").__setDimensions(width, 736);
+    const component = renderer.create(
+      <RView
+        style={{
+          backgroundColor: "black"
+        }}
+        rStyle={{
+          [MediaRule.MaxWidth]: {
+            300: {
+              backgroundColor: "red"
+            },
+            600: {
+              backgroundColor: "green"
+            },
+            900: {
+              backgroundColor: "blue"
+            }
+          },
+          [MediaRule.MinWidth]: {
+            200: {
+              padding: 33
+            },
+            500: {
+              padding: 66
+            },
+            800: {
+              padding: 99
             }
           }
         }}
