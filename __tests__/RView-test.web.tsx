cbbd57364dbbff4ebf8a0c3904f4b58380b59ae6
Yifei He <yifei@hesyifei.com>
2019-09-06 14:43:22 -0700

    [test] Add test for web

---
+++ __tests__/RView-test.web.tsx
@@ -0,0 +1,26 @@
+import renderer from "react-test-renderer";
+import {
+  simpleRView,
+  maxWidthRView,
+  maxWidthAndMinWidthRView
+} from "./RView-components";
+
+describe("RView", () => {
+  test("simple", () => {
+    const component = renderer.create(simpleRView);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("simple MaxWidth", () => {
+    const component = renderer.create(maxWidthRView);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  test("simple MinWidth and MaxWidth", () => {
+    const component = renderer.create(maxWidthAndMinWidthRView);
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
